2c11877775cb3f282da37c43a3166d7e520f0cae
MarioKrcelic <Mario.Krcelic@gmail.com>
2017-03-19 19:12:31 +0100

    Lab3

---
+++ PotapanjeBrodova/Test/TestPolja.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PotapljanjeBrodova;
+
+namespace Test
+{
+    [TestClass]
+    public class TestPolja
+    {
+        [TestMethod]
+        public void Polje_RedakIStupacJednakiSuArgumentimaKonstruktora()
+        {
+            Polje p = new Polje(2, 3);
+            Assert.AreEqual(2, p.Redak);
+            Assert.AreEqual(3, p.Stupac);
+        }
+
+        [TestMethod]
+        public void Polje_ZaDvaPoljaKojaImajuIsteKoordinateMetodaEqualsVracaTrue()
+        {
+            Polje p1 = new Polje(2, 3);
+            Polje p2 = new Polje(2, 3);
+            Assert.AreEqual(2, p2.Redak);
+            Assert.AreEqual(3, p2.Stupac);
+
+        }
+    }
+}
