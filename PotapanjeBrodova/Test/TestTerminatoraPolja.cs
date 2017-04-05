d32e64f5a3c2218f355d1a7979a5d3ea3ad4d2c2
Mario Krcelic <makrceli@vsite.hr>
2017-04-05 21:10:26 +0200

    Dodan i terminator

---
+++ PotapanjeBrodova/Test/TestTerminatoraPolja.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PotapanjeBrodova;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Test
+{
+    [TestClass]
+    public class TestTerminatoraPolja
+    {
+        private Mreža mreža;
+        private TerminatoraPolja terminator;
+
+        [TestInitialize()]
+        public void PripremiMrežuTerminatora()
+        {
+            mreža = new Mreža(10,10);
+            terminator = new TerminatoraPolja(mreža);
+        }
+
+        [TestMethod]
+        public void TestTerminatorPolja_UklanjaSvaPoljaOkoBrodaUSrediniMreže()
+        {
+            IEnumerable<Polje> polja = new Polje[] { new Polje(3, 3), new Polje(3, 4) };
+            terminator.UkloniPolja(polja);
+            Assert.AreEqual(88,mreža.DajSlobodnaPolja().Count());
+
+            //Dodaj provjeru da su izbačeni (3,3) i (3,4), (2,2), (2,5), (4,2), (4,5)
+        }
+
+        [TestMethod]
+        public void TestTerminatorPolja_UklanjaSvaPoljaOkoBrodaUzGornjiRubMreže()
+        {
+        }
+
+        [TestMethod]
+        public void TestTerminatorPolja_UklanjaSvaPoljaOkoBrodaUGornjemDesnomKutuMreže()
+        {
+        }
+        [TestMethod]
+        public void TestTerminatorPolja_UklanjaSvaPoljaOkoBrodaUGornjemDLijevomKutuMreže()
+        {
+        }
+
+        [TestMethod]
+        public void TestTerminatorPolja_UklanjaSvaPoljaOkoBrodaUGDonjemDesnomKutuMreže()
+        {
+        }
+
+        [TestMethod]
+        public void TestTerminatorPolja_UklanjaSvaPoljaOkoBrodaUGDonjemLijevomKutuMreže()
+        {
+        }
+    }
+}
