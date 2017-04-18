7b8448c87a4e04d154a9e696151b17bcf395ffdb
Mario Krcelic <makrceli@vsite.hr>
2017-04-18 20:56:24 +0200

    Brodograditelj i test

--- PotapanjeBrodova/PotapanjeBrodova/Mreža.cs
+++ PotapanjeBrodova/PotapanjeBrodova/Mreža.cs
@@ -95,7 +95,8 @@ namespace PotapanjeBrodova
 
 
         private Polje[,] polja;
-        private int redaka;
-        private int stupaca;
+
+        public readonly int redaka;
+        public readonly int stupaca;
     }
 }
