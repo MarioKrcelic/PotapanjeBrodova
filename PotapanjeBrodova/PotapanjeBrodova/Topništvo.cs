6c59b0524721addb8339c3adbe1b1191c825d11d
Mario Krcelic <makrceli@vsite.hr>
2017-05-03 20:49:46 +0200

    Dodano Topništvo

---
+++ PotapanjeBrodova/PotapanjeBrodova/Topništvo.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace PotapanjeBrodova
+{
+    public enum TaktikaGađanja
+    {
+        Nasumično,
+        Kružno,
+        Linijsko
+    }
+
+    public class Topništvo
+    {
+
+        public Topništvo(int redaka, int stupaca, IEnumerable<int> duljineBrodova)
+        {
+
+            mreža = new Mreža(redaka, stupaca);
+            this.duljineBrodova = new List<int>(duljineBrodova);
+            TaktikaGađanja = TaktikaGađanja.Nasumično;
+        }
+
+        public void ObradiGađanje(RezultatGađanja rezultat)
+        {
+            if (rezultat == RezultatGađanja.Promašaj) return;
+
+            if(rezultat == RezultatGađanja.Pogodak)
+            {
+                switch(TaktikaGađanja)
+                {
+
+                    case TaktikaGađanja.Nasumično:
+                        PromijeniTaktikuUKružnu();
+                        return;
+                    case TaktikaGađanja.Kružno:
+                        PromijeniTaktikuULinijsku();
+                        return;
+                    case TaktikaGađanja.Linijsko:
+                        return;
+                    default:
+                        Debug.Assert(false);
+                        break;
+                        
+                }
+            }
+
+            Debug.Assert(rezultat == RezultatGađanja.Potopljen);
+            PromijeniTaktikuUNasumično();
+
+        }
+
+       void PromijeniTaktikuUKružnu()
+        {
+            TaktikaGađanja = TaktikaGađanja.Kružno;
+        }
+
+        void PromijeniTaktikuULinijsku()
+        {
+            TaktikaGađanja = TaktikaGađanja.Linijsko;
+        }
+
+        void PromijeniTaktikuUNasumično()
+        {
+            TaktikaGađanja = TaktikaGađanja.Nasumično;
+        }
+
+
+        public TaktikaGađanja TaktikaGađanja { get; private set; }
+
+        private Mreža mreža;
+        private List<int> duljineBrodova;
+        
+    }
+}
