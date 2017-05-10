fb68a93ebcc47b6179115f70c4c312ce8193395b
Mario Krcelic <makrceli@vsite.hr>
2017-05-10 09:22:39 +0200

    Ispravak

---
+++ PotapanjeBrodova/PotapanjeBrodova/IGađani.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PotapanjeBrodova
+{
+
+    public enum RezultatGađanja
+    {
+        Promašaj,
+        Pogodak,
+        Potopljen
+    }
+    interface IGađani
+    {
+
+        RezultatGađanja Gađaj(Polje polje);
+
+    }
+}
--- PotapanjeBrodova/Test/TestBroda.cs
+++ PotapanjeBrodova/Test/TestBroda.cs
@@ -2,6 +2,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PotapanjeBrodova;
 using System.Linq;
+
 namespace Test
 {
     [TestClass]
@@ -14,7 +15,61 @@ namespace Test
             Assert.AreEqual(2, brod.Polja.Count());
             Assert.IsTrue(brod.Polja.Contains(new Polje(1, 1)));
             Assert.IsTrue(brod.Polja.Contains(new Polje(1, 2)));
+        }
 
+        [TestMethod]
+        public void Brod_GađajVraćaPromašajZaPoljeKojeNePripadaBrodu()
+        {
+            Brod brod = new Brod(new Polje[] { new Polje(1, 1), new Polje(1, 2) });
+            RezultatGađanja rez = brod.Gađaj(new Polje(0, 0));
+            Assert.AreEqual(RezultatGađanja.Promašaj, rez);
+        }
+
+        [TestMethod]
+        public void Brod_GađajVraćaPogodakZaPrvoPogođenoPolje()
+        {
+            Brod brod = new Brod(new Polje[] { new Polje(1, 1), new Polje(1, 2), new Polje(1, 3) });
+            RezultatGađanja rez = brod.Gađaj(new Polje(1, 2));
+            Assert.AreEqual(RezultatGađanja.Pogodak, rez);
+        }
+
+        [TestMethod]
+        public void Brod_GađajVraćaPogodakZaDrugoPogođenoPoljeBrodaOdTriPolja()
+        {
+            Brod brod = new Brod(new Polje[] { new Polje(1, 1), new Polje(1, 2), new Polje(1, 3) });
+            brod.Gađaj(new Polje(1, 2));
+            RezultatGađanja rez = brod.Gađaj(new Polje(1, 1));
+            Assert.AreEqual(RezultatGađanja.Pogodak, rez);
+        }
+
+        [TestMethod]
+        public void Brod_GađajVraćaPotopljenZaTrećePogođenoPoljeBrodaOdTriPolja()
+        {
+            Brod brod = new Brod(new Polje[] { new Polje(1, 1), new Polje(1, 2), new Polje(1, 3) });
+            brod.Gađaj(new Polje(1, 2));
+            brod.Gađaj(new Polje(1, 3));
+            RezultatGađanja rez = brod.Gađaj(new Polje(1, 1));
+            Assert.AreEqual(RezultatGađanja.Potopljen, rez);
+        }
+
+        [TestMethod]
+        public void Brod_GađajVraćaPogodakZaDrugoGađanjeIstogPolja()
+        {
+            Brod brod = new Brod(new Polje[] { new Polje(1, 1), new Polje(1, 2), new Polje(1, 3) });
+            brod.Gađaj(new Polje(1, 2));
+            RezultatGađanja rez = brod.Gađaj(new Polje(1, 2));
+            Assert.AreEqual(RezultatGađanja.Pogodak, rez);
+        }
+
+        [TestMethod]
+        public void Brod_GađajVraćaPotopljenZaDrugoGađanjePotopljenogBroda()
+        {
+            Brod brod = new Brod(new Polje[] { new Polje(1, 1), new Polje(1, 2), new Polje(1, 3) });
+            //brod.Gađaj(new Polje(1, 1));
+            brod.Gađaj(new Polje(1, 2));
+            brod.Gađaj(new Polje(1, 3));
+            RezultatGađanja rez = brod.Gađaj(new Polje(1, 1));
+            Assert.AreEqual(RezultatGađanja.Potopljen, rez);
         }
     }
 }
